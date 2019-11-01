d0e19616bc9f4d1442d7a70c65c63973d0107864
molysama <molysama.i@gmail.com>
2019-11-01 21:27:13 +0800

    添加多入口配置

--- dist/types/index.d.ts
+++
@@ -1,6 +0,0 @@
-import * as core from '@auto.pro/core';
-declare const cap: typeof core.cap;
-declare const click: (x: number, y: number, delay?: [number, number] | [600, 800]) => any;
-declare const swipe: (startPoint: [number, number], endPoint: [number, number], duration?: number) => any;
-export { cap, click, swipe };
-//# sourceMappingURL=index.d.ts.map
