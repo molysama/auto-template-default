a5ec20bf7a3888c41b69514e9c46e44a0361fc74
molysama <molysama.i@gmail.com>
2020-12-29 11:10:47 +0800

    更新依赖

--- src/index.js
+++ src/index.js
@@ -6,17 +6,21 @@ function show(hello, world = 'world') {
 const hello = 'hello'
 const result = show(hello)
 
-ui.layout(
+ui.layout(`
     <vertical>
-        <button id="btn1" text="{{result}}" />
-        <button id="btn2" text="按钮{{hello}}" />
+        <button id="btn1" text="${result}" />
+        <button id="btn2" text="按钮${hello}" />
+        <drawer id="drawer" ></drawer>
     </vertical>
-)
+`)
 
 ui.btn1.on('click', () => {
     toastLog(result)
 })
 
-ui.btn2.on('click', () => {
-    toast(hello)
+// ui.btn2.on('click', () => {
+//     toast(hello)
+// })
+ui.btn2.on('touch', (e) => {
+    log(e)
 })
